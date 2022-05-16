7e851b064363b3e54f3dcaf273df21a4d17bf3b2
YTP <woodolly17@gmail.com>
2022-05-17 02:14:01 +0900

    multiple circles using sin,cos

--- GLSLBase/Shaders/FSSandbox.fs
+++ GLSLBase/Shaders/FSSandbox.fs
@@ -4,9 +4,14 @@ layout(location=0) out vec4 FragColor;
 
 in vec4 v_Color;
 
+const float PI = 3.141592;
+
 vec4 CrossPattern()
 {
     vec4 returnValue = vec4(1, 1, 1, 1);
+    float X = cos(v_Color.x * 20 * PI);
+    float Y = cos(v_Color.y * 20 * PI);
+    returnValue = vec4(max(X, Y));
     return returnValue;
 }
 
@@ -28,21 +33,34 @@ float dis = distance(v_Color.xy, vec2(0.5, 0.5));
     return newColor;
 }
 
-void main()
+vec4 DrawMultipleCircles()
+{
+    float dis = distance(v_Color.xy, vec2(0.5, 0.5));
+    float temp = cos(dis*40*PI);
+    return vec4(temp);
+}
+
+vec4 DrawCircleLine()
 {
     float dis = distance(v_Color.xy, vec2(0.5, 0.5));
 
     vec4 newColor = vec4(0,0,0,0);
-
-    if( dis < 0.5)
+    if(dis > 0.45 && dis < 0.5)
     {
-        newColor = vec4(0,0,0,0);
+        newColor = vec4(1);
     }
     else
     {
-        newColor = vec4(1,1,1,1);
+        newColor = vec4(0);
     }
 
+    return newColor;
+}
+
+void main()
+{
     //FragColor = CrossPattern();
-    FragColor = DrawCircle();
+    //FragColor = DrawCircle();
+    //FragColor = DrawCircleLine();
+    FragColor = DrawMultipleCircles();
 }
